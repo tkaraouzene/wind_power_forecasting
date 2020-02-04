bf78609cb0c1fb0c8ffe2ebd259fd2c882031037
SESA494757 <thomas.karaouzene@non.se.com>
2020-02-04 15:46:58 +0100

    fix import bug

--- wind_power_forecasting/__init__.py
+++ wind_power_forecasting/__init__.py
@@ -4,11 +4,13 @@
 DATA_DIR = '../data/'
 RAW_DATA_DIR = DATA_DIR + 'raw/'
 X_TRAIN_FILE = RAW_DATA_DIR + 'X_train_v2.csv'
+Y_TRAIN_FILE = RAW_DATA_DIR + 'Y_train_sl9m6Jh.csv'
 X_TEST_FILE = RAW_DATA_DIR + 'X_test_v2.csv'
 
 
 # Input data labels
 
 TIME_LABEL = 'Time'
-TARGET_LABEL = ''
+TARGET_LABEL = 'Production'
+ID_LABEL = 'ID'
 WF_LABEL = 'WF'
--- wind_power_forecasting/main.py
+++ wind_power_forecasting/main.py
@@ -1,19 +1,21 @@
 import pandas as pd
 
-from wind_power_forecasting import X_TRAIN_FILE, TIME_LABEL, WF_LABEL
+from wind_power_forecasting import X_TRAIN_FILE, TIME_LABEL, WF_LABEL, Y_TRAIN_FILE, ID_LABEL
 from wind_power_forecasting.model.wind_power_forcaster import WindPowerForecaster
 from wind_power_forecasting.utils import get_sub_df
 
 if __name__ == '__main__':
 
     X_all_df = pd.read_csv(X_TRAIN_FILE)
+    y_all_df = pd.read_csv(Y_TRAIN_FILE)
 
     for wf in X_all_df[WF_LABEL].unique():
         X_wf_df = get_sub_df(X_all_df, WF_LABEL, wf)
+        y_wf_df = get_sub_df(y_all_df, ID_LABEL, X_wf_df[ID_LABEL])
 
         wpf = WindPowerForecaster(target_label = 'ID', datetime_label=TIME_LABEL)
 
-        wpf.fit(X_wf_df, None)
+        wpf.fit(X_wf_df, y_wf_df)
 
         pass
     pass
