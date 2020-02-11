067d65f1fc6968855655dfc57cd0b0d5b2ef9b89
SESA494757 <thomas.karaouzene@non.se.com>
2020-02-11 13:18:05 +0100

    add wind features

--- wind_power_forecasting/features_extraction/weather/wind.py
+++ wind_power_forecasting/features_extraction/weather/wind.py
@@ -4,17 +4,55 @@ from sklearn.utils import check_consistent_length, check_array
 from wind_power_forecasting.utils.dataframe import copy_or_not_copy
 
 
-def add_wind_speed(df, meridional_velocity_label, zonal_velocity_label, wind_speed_label='wind_speed', copy=True):
+def add_wind_speed(df, u_label, v_label, wind_speed_label='wind_speed', copy=True):
     df = copy_or_not_copy(df, copy)
 
-    df[wind_speed_label] = compute_wind_speed(df[meridional_velocity_label], df[zonal_velocity_label])
+    df[wind_speed_label] = compute_wind_speed(df[u_label], df[v_label])
 
     return df
 
 
-def compute_wind_speed(meridional_velocity, zonal_velocity):
-    meridional_velocity = check_array(meridional_velocity, ensure_2d=False, force_all_finite=False)
-    zonal_velocity = check_array(zonal_velocity, ensure_2d=False, force_all_finite=False)
-    check_consistent_length(meridional_velocity, zonal_velocity)
+def add_wind_vector_azimuth(df, u_label, v_label, wind_speed_label='wind_vector_azimuth', copy=True):
+    df = copy_or_not_copy(df, copy)
 
-    return np.sqrt(np.square(meridional_velocity) + np.square(zonal_velocity))
+    df[wind_speed_label] = compute_wind_vector_azimuth(df[u_label], df[v_label])
+
+    return df
+
+
+def add_meteorological_wind_direction(df, u_label, v_label, wind_speed_label='meteorological_wind_direction', copy=True):
+    df = copy_or_not_copy(df, copy)
+
+    df[wind_speed_label] = compute_meteorological_wind_direction(df[u_label], df[v_label])
+
+    return df
+
+
+def compute_wind_speed(u, v):
+    u = check_array(u, ensure_2d=False, force_all_finite=False)
+    v = check_array(v, ensure_2d=False, force_all_finite=False)
+    check_consistent_length(u, v)
+
+    return np.sqrt(np.square(u) + np.square(v))
+
+
+def compute_wind_vector_azimuth(u, v):
+    u = check_array(u, ensure_2d=False, force_all_finite=False)
+    v = check_array(v, ensure_2d=False, force_all_finite=False)
+    check_consistent_length(u, v)
+    return np.degrees(np.arctan2(u, v))
+
+
+def compute_meteorological_wind_direction(u, v):
+    u = check_array(u, ensure_2d=False, force_all_finite=False)
+    v = check_array(v, ensure_2d=False, force_all_finite=False)
+    check_consistent_length(u, v)
+    return np.degrees(np.arctan2(-u, -v))
+
+
+def wind_vector_azimuth_to_meteorological_wind_direction(wind_vector_azimuth):
+    return wind_vector_azimuth + 180
+
+
+def meteorological_wind_direction_to_wind_vector_azimuth(meteorological_wind_direction):
+    return meteorological_wind_direction - 180
